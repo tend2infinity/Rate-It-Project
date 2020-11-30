097160501766c35551b09916528450f1de7b965e
tend2infinity <somu12.ss@gmail.com>
2020-11-30 21:15:13 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React , {useEffect, useState} from 'react';
+import './App.css';
+import Navbar from './Navbar.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <h1> Let us create something good !</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+import React, { useContext,useRef,useEffect,useState } from 'react'
+import {Link} from 'react-router-dom'
+import Button from '@material-ui/core/Button'
+
+const Navbar= ()=> {
+    
+    return (
+        <div>
+            <Button variant="contained" color="primary">Hello bhaisaab 
+             </Button>
+        </div>
+    )
+}
+
+export default Navbar
