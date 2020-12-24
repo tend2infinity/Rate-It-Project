1c8ab11f8dc7e13e6f430ebf4eabd2560992e15f
tend2infinity <somu12.ss@gmail.com>
2020-12-24 12:19:27 +0530

    made protected routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import React , {useEffect, useState} from 'react';
 import {Home, Browse, Signin, Signup } from './pages'
 import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { IsUserRedirect, ProtectedRoute } from './handleUser/routes';
+import {useAuthListener} from './hooks';
 
 
 export function App() {
+  const {user} = useAuthListener();
+  console.log(user);
   return (
     <Router>
-      <Route exact path="/signup">
-        <Signup />
-      </Route> 
-      <Route exact path="/signin">
-        <Signin />
-      </Route>
-      <Route exact path="/browse">
-        <Browse />
-      </Route>
-      <Route exact path="/">
-        <Home />
-      </Route>
+      
+      <IsUserRedirect user={user} loggedInPath="/browse" path="/" exact>
+      <Home />
+      </IsUserRedirect>
+
+      <IsUserRedirect user={user} loggedInPath="/browse" path="/signin" exact>
+          <Signin />
+      </IsUserRedirect>
+
+      <IsUserRedirect user={user} loggedInPath="/browse" path="/signup" exact>
+          <Signup />
+      </IsUserRedirect>
+      
+      <ProtectedRoute user={user} path="/browse" exact>
+      <Browse />
+      </ProtectedRoute>
+    
+   
+     
+      
        
     </Router>
   );
---
+++ src/hooks/use-content.js
@@ -0,0 +1,6 @@
+import { useEffect, useState, useContext} from 'react';
+import { FirebaseContext} from '../context/firebase';
+
+export default function useContent(target) {
+    
+}
