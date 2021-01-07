61da1fd549f85e9eab923b97f9790ae22cf072cf
savi <shambhavishandilya01@gmail.com>
2021-01-07 13:38:37 +0530

    Converting to function component

--- src/components/Movie-list/MovieDetails.js
+++ src/components/Movie-list/MovieDetails.js
@@ -1,106 +1,68 @@
 import React from 'react';
 import movieTrailer from 'movie-trailer';
 import './MovieDetails.css';
-import YouTube from 'react-youtube';
 
-export default class MovieDetails extends React.Component{
-	constructor(props) {
-    super(props);
-    this.state ={
-		trailer: null,
-		image: null,
-		show: "none",
-		nominated: false,
-	}
-}
+export default function MovieDetails(props){
 
-YouTubeGetID(url){
-  var ID = '';
-  url = url.replace(/(>|<)/gi,'').split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
-  if(url[2] !== undefined) {
-    ID = url[2].split(/[^0-9a-z_\-]/i);
-    ID = ID[0];
-  }
-  else {
-    ID = url;
-  }
-    return ID;
-};
+const [nominated, setNominated] = React.useState(false);
+const [trailer, setTrailer] = React.useState(null);
+const [show, setShow] = React.useState("none");
 
-componentDidMount(){
-	const path1 = "https://image.tmdb.org/t/p/original";
-    const path2 = String(this.props.Poster);
-    var path3 = null;
-    path3 = path1.concat(path2);
-	movieTrailer( String(this.props.title ))
-        .then( response =>
-        {
-			response = this.YouTubeGetID(response);
-			this.setState({trailer: response})
-			this.setState({image: path3})
-		}
-	)
-};
 
-showModal()
+
+
+const showModal = () => 
 {
-	this.setState({show: "block"});
-	movieTrailer( this.props.title )
+	setShow("block");
+	movieTrailer( props.title )
         .then( response =>
         {
-			response = this.YouTubeGetID(response);
-			this.setState({trailer: response});
+			response = response.replace("watch?v=", "embed/")
+			setTrailer(response);
 		}
         )
 };
 
-closeModal()
+const closeModal = () => 
 {
-	this.setState({trailer: ""});
-	this.setState({show: "none"});
+	setTrailer("");
+	setShow("none");
 };
 
-handleNomination()
+const handleNomination = () => 
 {
-	this.setState({nominated: !(this.state.nominated)});
+	setNominated(!(nominated));
 };
 
-  
-	render() {
-		
-		
-const opts = {
-      height: '400',
-      width: '800',
-      playerVars: {
-        autoplay: 0,
-      },
-    };
+
+
+
+
 
     return (
-    <div className="outer" style = {{backgroundImage: `url(https://image.tmdb.org/t/p/original${this.props.Poster})`}}>
+    <div className="outer" style = {{backgroundImage: `url(https://image.tmdb.org/t/p/original${props.Poster})`}}>
     
     <div className="left">
     
-    <h1>{this.props.title}</h1>
+    <h1>{props.title}</h1>
     
-    <h2><b>Date of Release: </b>{this.props.date}</h2>
+    <h2><b>Date of Release: </b>{props.date}</h2>
     
     <br></br>
     
-    <h2><b>Storyline: </b><br></br>{this.props.Overview}</h2>
+    <h2><b>Storyline: </b><br></br>{props.Overview}</h2>
     <div className = "box">
     
-    <button id="myBtn" className="trailer-button"  onClick={this.showModal.bind(this)}>Watch Trailer</button>
+    <button id="myBtn" className="trailer-button"  onClick={showModal}>Watch Trailer</button>
     
-    <button id="myBtn" className="trailer-button"  onClick={this.handleNomination.bind(this)}>{(this.state.nominated)?"Remove":"Nominate"}</button>
+    <button id="myBtn" className="trailer-button"  onClick={handleNomination}>{(nominated)?"Remove":"Nominate"}</button>
     </div>
     
-    <div id="myModal" className="modal" style={{display: this.state.show}}>
+    <div id="myModal" className="modal" style={{display: show}}>
     
     <div className="modal-content">
-    <span className="close" onClick={this.closeModal.bind(this)}>&times;</span>
-    <YouTube videoId={this.state.trailer} opts={opts}/>
+    <span className="close" onClick={closeModal}>&times;</span>
+    <iframe src={trailer} width="100%" height="500px"/>
     </div>
 
    </div>
@@ -110,10 +72,9 @@ const opts = {
    
    
    <div className="right">
-   <img src={`https://image.tmdb.org/t/p/original${this.props.Poster}`}></img>
+   <img src={`https://image.tmdb.org/t/p/original${props.Poster}`}></img>
    </div>
    
    </div>
     );
-  }
 }
